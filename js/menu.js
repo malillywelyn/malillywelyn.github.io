0ffd72faa13c50b68d19270188269f724568c963
Chris Woolf <chriswoolf@mail.com>
2019-03-16 03:00:28 +0000

    mobile tweak

---
+++ js/menu.js
@@ -0,0 +1,16 @@
+ /* Click 1 */
+ $(".line1").click(function() {
+    var click = $(this).data("click");
+    if (click) {
+    $("nav").animate({ 
+        top: "-=120vh"
+    }, 900 );
+
+    } else {
+    
+    $("nav").animate({ 
+        top: "+=120vh"
+    }, 900 ); }
+    $(this).data("click", !click);
+    });
+
