a781b10cb5fde15c48d0cec3f3275778ecdba6d7
Chris Woolf <chriswoolf@mail.com>
2019-03-18 07:23:18 +0000

    tweak for gallery

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-$(function() {
+
+
+  $(function() {
     $('.main-title').animate({ 
         left: "+=100vw",
     }, 1200 );
@@ -10,3 +12,82 @@ $(function() {
   }, 1200 );
 });
 
+  /*Arrow 1 - Video*/
+  $(".video1").click(function() {
+    var click = $(this).data("click");
+    if (click) {
+    $(".vid1").animate({ 
+        opacity: 0,
+    }, 900 );
+
+    } else {
+
+    $(".vid1").animate({ 
+        opacity: 1,
+    }, 900 ); }
+    $(this).data("click", !click);
+    });
+
+         /*Arrow 2 - Video*/
+ $(".video2").click(function() {
+    var click = $(this).data("click");
+    if (click) {
+    $(".vid2").animate({ 
+        opacity: 0,
+    }, 900 );
+
+    } else {
+
+    $(".vid2").animate({ 
+        opacity: 1,
+    }, 900 ); }
+    $(this).data("click", !click);
+    });
+
+         /*Arrow 1 - Video*/
+ $(".video3").click(function() {
+    var click = $(this).data("click");
+    if (click) {
+    $(".vid3").animate({ 
+        opacity: 0,
+    }, 900 );
+
+    } else {
+
+    $(".vid3").animate({ 
+        opacity: 1,
+    }, 900 ); }
+    $(this).data("click", !click);
+    });
+
+         /*Arrow 1 - Video*/
+ $(".video4").click(function() {
+    var click = $(this).data("click");
+    if (click) {
+    $(".vid4").animate({ 
+        opacity: 0,
+    }, 900 );
+
+    } else {
+
+    $(".vid4").animate({ 
+        opacity: 1,
+    }, 900 ); }
+    $(this).data("click", !click);
+    });
+
+         /*Arrow 5 - Video*/
+ $(".video5").click(function() {
+    var click = $(this).data("click");
+    if (click) {
+    $(".vid5").animate({ 
+        opacity: 0,
+    }, 900 );
+
+    } else {
+
+    $(".vid5").animate({ 
+        opacity: 1,
+    }, 900 ); }
+    $(this).data("click", !click);
+    });
