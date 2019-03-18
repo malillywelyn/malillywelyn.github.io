0a1d5db5ffdcf1e984c9354de5f2cf55324c9960
Chris Woolf <chriswoolf@mail.com>
2019-03-18 05:37:27 +0000

    mobile menu color change

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@ $(function() {
     $('.main-title').animate({ 
         left: "+=100vw",
     }, 1200 );
+    $('.main-subtitle').animate({ 
+        left: "+=100vh",
+    }, 1200 );
     $('.main-img img').animate({ 
         left: "-=100vw",
     }, 1200 );
 });
-
--- js/transitions.js
+++ js/transitions.js
@@ -28,6 +28,9 @@ $("nav a").on("click", function (event) {
     $('.main-title').animate({ 
         left: "+=100vw",
     }, 1200 );
+    $('.main-subtitle').animate({ 
+      left: "+=100vw",
+  }, 1200 );
     $('.main-img img').animate({ 
       left: "-=100vw",
   }, 1200 );
